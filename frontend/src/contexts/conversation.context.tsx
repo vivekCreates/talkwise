ac1b7fe6520c1c17a9723b1fbf80b0978eec2ce2
vivek <codecrafter67@gmail.compwd>
2025-09-06 17:26:58 +0530

    feat: Implement conversation management with API integration and loading state handling

--- frontend/src/contexts/conversation.context.tsx
+++ frontend/src/contexts/conversation.context.tsx
@@ -1,7 +1,13 @@
-import { createContext } from "react";
+import React, { createContext, useContext, useState } from "react";
+import toast from "react-hot-toast";
+import { 
+  createConversationApi,
+  deleteConversationApi,
+  getAllConversationsApi,
+  getConversationApi } from "../api/conversation";
 
 export interface IConversation {
-  id: string;   // lowercase string
+  id: string;
   title: string;
   userId: string;
   createdAt: Date;
@@ -10,22 +16,104 @@ export interface IConversation {
 
 type ConversationContextType = {
   conversations: IConversation[] | null;
-  setConversations: (convs: IConversation[] | []) => void;
-  loading: boolean;
+  selectedConversation: IConversation | null;
+  isLoading: boolean;
   createConversation: (title: string) => Promise<void>;
-  getConversation: (id: string) => Promise<void>;
   getAllConversations: () => Promise<void>;
+  getConversation: (id: string) => Promise<void>;
   deleteConversation: (id: string) => Promise<void>;
 };
 
 const ConversationContext = createContext<ConversationContextType>({
   conversations: [],
-  setConversations: () => {},
-  loading: false,
-  createConversation: async () => {},
-  getConversation: async () => {},
-  getAllConversations: async () => {},
-  deleteConversation: async () => {},
+  selectedConversation: null,
+  isLoading: false,
+  createConversation: async () => { },
+  getConversation: async () => { },
+  getAllConversations: async () => { },
+  deleteConversation: async () => { },
 });
 
-export default ConversationContext;
+export const useConversation = () => useContext(ConversationContext)
+
+const ConversationProvider: React.FC<{ children: React.ReactNode }> =
+  ({ children }) => {
+
+    const [conversations, setConversations] = useState<IConversation[]>([]);
+    const [selectedConversation, setSelectedConversation] = useState<IConversation | null>(null);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
+
+    const createConversation = async (title: string) => {
+      setIsLoading(true)
+      try {
+        const res = await createConversationApi(title);
+        if (!res.success) {
+          toast.error(res.message)
+        }
+        toast.success(res.message)
+      } catch (error: any) {
+        toast.error(error?.message)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+
+    const getAllConversations = async () => {
+      setIsLoading(true)
+      try {
+        const res = await getAllConversationsApi();
+        if (!res.success) {
+          setConversations([])
+          return;
+        }
+        setConversations(res.data);
+      } catch (error: any) {
+        toast.error(error?.message)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+
+    const getConversation = async (id: string) => {
+      setIsLoading(true)
+      try {
+        const res = await getConversationApi(id);
+        if (!res.success) {
+           toast.error(res.message);
+           return
+        }
+        setSelectedConversation(res.data);
+        toast.success(res.message)
+      } catch (error: any) {
+        toast.error(error?.message)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+
+    const deleteConversation = async (id: string) => {
+      setIsLoading(true)
+      try {
+        const res = await deleteConversationApi(id);
+
+        if (!res.success){
+        toast.error(res.message);
+        return;
+        }
+        setConversations(prev => ([...prev.filter(p => p.id !== id)]))
+        toast.success(res.message)
+      } catch (error: any) {
+       toast.error(error?.message)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+
+    return <ConversationContext.Provider value={{ conversations, selectedConversation, isLoading, createConversation, getAllConversations,getConversation,deleteConversation }}>
+      {children}
+    </ConversationContext.Provider>
+  }
+
+
+export default ConversationProvider;
